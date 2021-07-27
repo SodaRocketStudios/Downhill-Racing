37128b175939e8191ad49308524c0067682e1fa9
renckbryce <renckbryce@gmail.com>
2021-07-27 10:41:14 -0600

    Converted Bezier curves to work in any number of dimesnions.

--- Assets/Prototype/BezierCurve/Bezier.cs
+++ Assets/Prototype/BezierCurve/Bezier.cs
@@ -6,36 +6,38 @@ namespace Curve
 {
     public static class Bezier
     {
-        public static Vector3[] GetCurve(Vector3 A, Vector3 B, Vector3 C, Vector3 D)
+        public static Vector3[] GetCurve(Vector3[] controlPoints, float resolution = 0.02f)
         {
-            float resolution = 0.02f;
-
             Vector3[] curvePoints = new Vector3[Mathf.RoundToInt(1f/resolution) + 1];
 
             int i = 0;
             for(float t = 0; t <= 1; t += resolution, i++)
             {
-                curvePoints[i] = GetPoint(A, B, C, D, t);
+                curvePoints[i] = GetPoint(controlPoints, t);
             }
 
             return curvePoints;
         }
 
-        private static Vector3 GetPoint(Vector3 A, Vector3 B, Vector3 C, Vector3 D, float t)
+        private static Vector3 GetPoint(Vector3[] controlPoints, float t)
         {
-            // Linear interpolation
-            Vector3 l0 = Vector3.Lerp(A, B, t);
-            Vector3 l1 = Vector3.Lerp(B, C, t);
-            Vector3 l2 = Vector3.Lerp(C, D, t);
+            int numberOfPoints = controlPoints.Length;
+            int dimesion = numberOfPoints - 1;
 
-            // Quadratic interpolation
-            Vector3 Q0 = Vector3.Lerp(l0, l1, t);
-            Vector3 Q1 = Vector3.Lerp(l1, l2, t);
+            Vector3[,] interpolatedPoints = new Vector3[numberOfPoints, numberOfPoints];
+            for(int i = 0; i < numberOfPoints; i++)
+            {
+                interpolatedPoints[0, i] = controlPoints[i];
+            }
 
-            // Cubic interpolation
-            Vector3 C0 = Vector3.Lerp(Q0, Q1, t);
-
-            return C0;
+            for(int i = 0; i < dimesion; i++)
+            {
+                for(int j = 0; j < dimesion - i; j++)
+                {
+                    interpolatedPoints[i+1, j] = Vector3.Lerp(interpolatedPoints[i, j], interpolatedPoints[i, j+1], t);
+                }
+            }
+            return interpolatedPoints[dimesion, 0];
         }
     }    
 }
---
+++ Assets/Prototype/BezierCurve/CurveTest.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Curve
+{
+    public class CurveTest : MonoBehaviour
+    {
+        public Vector3[] controlPoints;
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.white;
+
+            for(int i = 0; i < controlPoints.Length; i++)
+            {
+                Gizmos.DrawCube(controlPoints[i], Vector3.one*1f);
+            }
+
+            Vector3[] curvePoints = Bezier.GetCurve(controlPoints);
+
+            for(int i = 0; i < curvePoints.Length - 1; i++)
+            {
+                Gizmos.DrawLine(curvePoints[i], curvePoints[i+1]);
+            }
+        }
+    }
+}
